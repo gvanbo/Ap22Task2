8785d098462b4a0c118a3cd45f53db72dcaf85f6
gvanbo <gvanbo@gmail.com>
2021-04-22 16:16:11 -0600

    Add project files.

---
+++ Ap22Task2/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿
+namespace Ap22Task2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnGenerate = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.BtnEvenSort = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BtnGenerate
+            // 
+            this.BtnGenerate.Location = new System.Drawing.Point(72, 43);
+            this.BtnGenerate.Name = "BtnGenerate";
+            this.BtnGenerate.Size = new System.Drawing.Size(75, 23);
+            this.BtnGenerate.TabIndex = 0;
+            this.BtnGenerate.Text = "Generate";
+            this.BtnGenerate.UseVisualStyleBackColor = true;
+            this.BtnGenerate.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(168, 43);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(491, 23);
+            this.textBox1.TabIndex = 3;
+            // 
+            // BtnEvenSort
+            // 
+            this.BtnEvenSort.Location = new System.Drawing.Point(72, 88);
+            this.BtnEvenSort.Name = "BtnEvenSort";
+            this.BtnEvenSort.Size = new System.Drawing.Size(75, 23);
+            this.BtnEvenSort.TabIndex = 4;
+            this.BtnEvenSort.Text = "Even Sort";
+            this.BtnEvenSort.UseVisualStyleBackColor = true;
+            this.BtnEvenSort.Click += new System.EventHandler(this.BtnEvenSort_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(168, 87);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(491, 23);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(168, 130);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(491, 23);
+            this.textBox3.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.BtnEvenSort);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.BtnGenerate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnGenerate;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button BtnEvenSort;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ Ap22Task2/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ap22Task2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int[] numbers = new int[25]; 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random x = new Random();
+            string numberstring = "";
+
+            //for loop (start, end, step
+            for (int number = 0; number < numbers.Length; number ++)
+            {
+                int num = x.Next(1, 101); //Generate a random number from 1-100
+                numbers[number] = num; // assign x to the numbers array at index [number]
+                numberstring += (numbers[number].ToString() + ", ");
+            }
+            textBox1.Text = numberstring;
+
+        }
+
+  
+        private void BtnEvenSort_Click(object sender, EventArgs e)
+        {
+            //textBox1.Text = Array.Sort(numbers);
+
+            for (int x = 0; x < numbers.Length; x++)
+            {
+                //if (numbers[x] % 2) 
+            }
+            string evenstring = "";
+            string oddstring = "";
+
+            foreach(int x in numbers)
+            {
+                if (x % 2 != 0) //find even numbers
+                {
+                    evenstring += x + ", ";
+                } else // left over numbers must be odd (because they are not even)
+                {
+                    oddstring += x + ", ";
+                }
+            }
+            textBox2.Text = evenstring;
+            textBox3.Text = oddstring;
+        }
+
+        //private void string DisplayNumbers(arra num)
+        //{
+
+        //}
+    }
+}
